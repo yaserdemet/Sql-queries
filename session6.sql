172d600f40d113134db807f38751db84f4aeec2f
yaserdemet <99739515+yaserdemet@users.noreply.github.com>
2022-04-18 00:20:02 +0300

    Update session6.sql
    Sql working directory

--- session6.sql
+++ session6.sql
@@ -4,3 +4,14 @@ CREATE TABLE products (product_no INTEGER, name TEXT,
                        discounted_price NUMERIC CHECK (discounted_price > 0));
                        
 CREATE TABLE users (id SERIAL NOT NULL, age INTEGER, email VARCHAR(50);
+
+                    
+/* Veri Güncelleme */
+                    
+Update author SET first_name = "Joseph" , last_name = "JACK" WHERE id = 1;
+
+UPDATE author SET last_name = "ANAR"  , age = 33 WHERE first_name LIKE ("%z") RETURNING *;
+                    
+/* Update yaparken mutlaka where ile nerenin güncelleneceğini belirtmek gerek 
+verisini güncelleyeceğimiz tablo , set ile güncellenecek sütunlar */  
+                    
